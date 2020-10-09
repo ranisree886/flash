40bae50e90f2bb849dd4bde3910c12ee0c132411
ranisree <72619216+ranisree886@users.noreply.github.com>
2020-10-09 22:43:05 +0530

    first commit uploading all files

---
+++ corejava/src/com/nisum/mainmethodques/Demo.java
@@ -0,0 +1,14 @@
+package com.nisum.mainmethodques;
+
+public class Demo {
+	void m1() {
+		System.out.println("from  m1");
+	}
+
+	public   static  void main(String[] args) {
+		Demo d=new Demo();
+		d.m1();
+
+	}
+
+}
